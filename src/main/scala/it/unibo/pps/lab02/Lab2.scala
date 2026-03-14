798667f479a9756c7a78ce458819d9e8a0d4eb26
Icobart <enrico.cornacchia@studio.unibo.it>
2026-03-14 14:29:21 +0100

    testing currying with divide operation

--- src/main/scala/it/unibo/pps/lab02/Lab2.scala
+++ src/main/scala/it/unibo/pps/lab02/Lab2.scala
@@ -3,3 +3,12 @@ package it.unibo.pps.lab02
 object Lab2 extends App:
   //Task 1, svolto da solo
   println("Hello, Scala")
+  def divide(x: Double, y: Double): Double = x / y
+  def divideCurried(x: Double)(y: Double): Double = x / y
+  println(divide(10, 5)) //2.0
+  println(divideCurried(10)(5)) //2.0
+  val divideCurriedAsFunction: Double => Double => Double = x => y => x / y
+  println(divideCurriedAsFunction(10)(5)) //2.0
+  val divideTenBy: Double => Double = divideCurried(10)
+  println(divideTenBy(5)) //2.0
+  println(divideTenBy(2)) //5.0
